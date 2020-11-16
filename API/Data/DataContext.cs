9c8eb0b843fb742a9df3dc6e9a227d2136bba3cc
Greg Brignola <GBrignola@kantarmedia.local>
2020-11-16 09:33:44 -0500

    end of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+using System.Diagnostics.CodeAnalysis;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
